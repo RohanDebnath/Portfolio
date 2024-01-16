2172f0cca2bddca0f8513ab37b2c86422d774b42
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-17 03:36:38 +0530

    skill section done

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ window.onscroll = () => {
     let header = document.querySelector('.header');
     header.classList.toggle('sticky', window.scrollY > 100);
 }
+
+document.querySelectorAll('.imgBx').forEach((element) => {
+    element.addEventListener('click', () => {
+        element.classList.toggle('active');
+        console.log('Active class toggled:', element.classList.contains('active'));
+    });
+});
