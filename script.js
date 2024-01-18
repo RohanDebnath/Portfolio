5debab9d3b8fbbb019a35cdc65f32fa857ae7499
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-18 21:01:34 +0530

    Project UI changed

--- script.js
+++ script.js
@@ -112,3 +112,84 @@ document.addEventListener('scroll', function () {
     
     document.getElementById('progress-bar').style.width = scrolled + '%';
 });
+
+// Scroll Section Active Link 
+
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header .navbar a');
+
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if (top >= offset && top < offset + height) {
+//             navLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+//             });
+//         };
+//     });
+// };
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+let header = document.querySelector('header');
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    if (top > 50) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+// Menu icon Navbar
+
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// Remove menu icon navbar when click navbar link (scroll)
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+
+// Dark Mode
+
+let darkModeIcon=document.querySelector('#darkMode-icon');
+darkModeIcon.onclick=()=>{
+    darkModeIcon.classList.toggle('bx-sun')
+    document.body.classList.toggle('dark-mode');
+}
+
+// Scoll Reveal
+ScrollReveal({ 
+    reset: true ,
+    distence:'80px',
+    duration:2000,
+    delay:200
+});
+
+// ScrollReveal().reveal('.home-content, .heading',{origin:'top'});
+// ScrollReveal().reveal('.home-img img',{origin:'bottom'});
